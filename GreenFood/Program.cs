83a481998c76ef9534bef0dfa218d5ba8613f452
Иван <vanjapo4ta@gmail.com>
2023-02-06 21:28:13 +0300

    Add docker compose

--- GreenFood/Program.cs
+++ GreenFood/Program.cs
@@ -3,4 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/Home", () => "Home!!!!");
+app.MapGet("/Ivan", () => "Hello Ivan!!!");
+app.MapGet("/GetData", () => "Something...");
 app.Run();
