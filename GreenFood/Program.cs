fef8985224b57d19ef1c45bd8cc9bacdc337ab5d
Иван <vanjapo4ta@gmail.com>
2023-02-07 23:21:10 +0300

    Fix of organization data in docker-compose & create class for Configuration functions.

--- GreenFood/Program.cs
+++ GreenFood/Program.cs
@@ -1,11 +1,9 @@
+using GreenFood.features;
 using Serilog;
-using Serilog.Exceptions;
-using Serilog.Sinks.Elasticsearch;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureLog();
+ConfigSettings.ConfigureLog(new ConfigurationBuilder());
 builder.Host.UseSerilog();
 
 var app = builder.Build();
@@ -14,26 +12,3 @@ app.MapGet("/", () => "World!");
 app.MapGet("/Home", () => "Home!");
 app.Run();
 
-void ConfigureLog()
-{
-    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-    var configuraton = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .Build();
-    Log.Logger = new LoggerConfiguration()
-        .Enrich.FromLogContext()
-        .Enrich.WithExceptionDetails()
-        .WriteTo.Debug()
-        .WriteTo.Elasticsearch(ConfigureELS(configuraton, env))
-        .CreateLogger();
-}
-
-ElasticsearchSinkOptions ConfigureELS(IConfigurationRoot configuraton, string env)
-{
-    return new ElasticsearchSinkOptions(new Uri(configuraton["ELKConfiguration:Uri"]))
-    {
-        AutoRegisterTemplate = true,
-        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}" // Странная запись
-    };
-}
---
+++ GreenFood/features/ConfigSettings.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
+namespace GreenFood.features
+{
+    public static class ConfigSettings
+    {
+        static public void ConfigureLog(ConfigurationBuilder config)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfigurationRoot configuraton = config
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .WriteTo.Debug()
+                .WriteTo.Elasticsearch(ConfigureELS(configuraton, env))
+                .CreateLogger();
+        }
+
+        static private ElasticsearchSinkOptions ConfigureELS(IConfigurationRoot configuraton, string env)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuraton["ELKConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
+    }
+}
