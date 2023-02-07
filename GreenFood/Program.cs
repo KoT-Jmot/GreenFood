4c1fddcb823a1135edec1e73df7b886333dbed8c
Иван <vanjapo4ta@gmail.com>
2023-02-07 21:50:03 +0300

    Add ELK and orgonize data in docker-compose

--- GreenFood/Program.cs
+++ GreenFood/Program.cs
@@ -1,8 +1,39 @@
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureLog();
+builder.Host.UseSerilog();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/Home", () => "Home!!!!");
-app.MapGet("/Ivan", () => "Hello Ivan!!!");
-app.MapGet("/GetData", () => "Something...");
+app.MapGet("/", () => "World!");
+app.MapGet("/Home", () => "Home!");
 app.Run();
+
+void ConfigureLog()
+{
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuraton = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Elasticsearch(ConfigureELS(configuraton, env))
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureELS(IConfigurationRoot configuraton, string env)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuraton["ELKConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}" // Странная запись
+    };
+}
