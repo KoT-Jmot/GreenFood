da2f2bc15f984f1ff342797c22a4eb1b5d01eeef
Q117 <vanjapo4ta@gmail.com>
2023-03-10 12:51:44 +0300

    Fix bug with order customer and product seller

---
+++ GreenFood.Domain/Exceptions/OrderCustomerException.cs
@@ -0,0 +1,9 @@
+﻿namespace GreenFood.Domain.Exceptions
+{
+    public class OrderCustomerException : Exception
+    {
+        public OrderCustomerException(string message="Incorrect customer of order!") : base(message) 
+        { 
+        }
+    }
+}
