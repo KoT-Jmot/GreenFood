dc9bd1eed7e247beb7818e249bc0311b5f0f64ff
Q117 <vanjapo4ta@gmail.com>
2023-03-19 17:56:30 +0300

    Add cors connections

--- Server/GreenFood.Web/Extensions/ServiceExtensions.cs
+++ Server/GreenFood.Web/Extensions/ServiceExtensions.cs
@@ -151,5 +151,20 @@ namespace GreenFood.Web.Extensions
 
             return services;
         }
+
+        public static IServiceCollection ConfigureCors(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "GreenFoodOrigins",policy =>
+                {
+                    policy.WithOrigins(configuration.GetSection("ClientInformations").GetValue<string>("link"));
+                });
+            });
+            
+            return services;
+        }
     }
 }
